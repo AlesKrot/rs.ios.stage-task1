fca98210d0a767007f0675dc2c1f7a07494d6a47
Ales Krot <ales.krot@gmail.com>
2021-05-10 20:41:46 +0300

    add: function in exercise 4

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,33 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    
+    NSMutableArray *arr = [[NSMutableArray alloc] init];
+
+    if ( number == nil ){
+        return arr;
+    }
+    
+    int a = number.intValue;
+    
+    if ( a < 0){
+        a *=-1;
+    }
+    
+    NSString *str = [@(a) stringValue];
+    
+    for ( int i = 0; i < str.length; ++i ){
+        [arr addObject:[NSString stringWithFormat:@"%C", [str characterAtIndex:i]]];
+    }
+    
+    if ( number < 0 ){
+        [arr removeObjectAtIndex:0];
+        return arr;
+    }
+        
+    NSArray *reversedArray = [[arr reverseObjectEnumerator] allObjects];
+    
+    return reversedArray;
 }
 
 @end
