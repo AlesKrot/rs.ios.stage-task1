b0ae491bf7d611b68d49b771ab374b0c8f20f9c1
Ales Krot <ales.krot@gmail.com>
2021-05-10 19:06:38 +0300

    add: function in exercise 2

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,20 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    
+    NSString *str = self;
+    
+    if ( str.length == 0 ){
+        return false;
+    }
+    
+    for ( int i = 0; i < str.length; ++i ){
+        char symb = [str characterAtIndex:i];
+        if (symb < '0' || symb > '9')
+            return false;
+    }
+  
+    return true;
 }
 
 @end
